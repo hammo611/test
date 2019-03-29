9cc5ffcae855405c2f9918b3da80370f34c4365d
hammo611 <48110597+hammo611@users.noreply.github.com>
2019-03-29 13:42:15 +1100

    Create main.c
    switch function example

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int add_Function(float x, float y);
+int opperator_Function(int o);
+
+int main()
+{
+    float x = 1; 
+    float y = 5;
+    int result;
+    
+    result = add_Function(x, y);
+    printf("%d", result);
+
+    return 0;
+}
+
+int add_Function(float x, float y)
+{
+    float result;
+    result = opperator_Function(void)
+     
+    return result;
+}
+
+void opperator_Function(int o)
+{
+    switch(o)
+    case 0: x+y;
+        break;
+    case 1: x-y;
+        break;
+    case 2: x*y;
+        break;
+    case 3: x/y;
+        break;
+    default: printf("error, illegal integer")
+
+    return void;
+    
+}
